e186cadb8917cf763817df5d1668b9b04ab8e1f9
jhon <jhon.bocanegra26@gmail.com>
2025-08-11 12:36:32 -0500

    feat: add hero data, interfaces, and custom pipes for enhanced functionality

--- src/app/pages/custom/custom.ts
+++ src/app/pages/custom/custom.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case-pipe';
+import { heroes } from '../../data/hero-data';
+import { CanFlyPipe } from '../../pipes/can-fly-pipe';
+import { HeroColorPipe } from '../../pipes/hero-color-pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color-pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroCreatorPipe } from '../../pipes/hero-creator-pipe';
 
 @Component({
   selector: 'app-custom',
-  imports: [],
+  imports: [
+    CanFlyPipe,
+    HeroColorPipe,
+    HeroCreatorPipe,
+    HeroTextColorPipe,
+    TitleCasePipe,
+    ToggleCasePipe,
+  ],
   templateUrl: './custom.html',
-  styles: ``
+  styles: ``,
 })
 export class Custom {
+  name = signal('Jhon Bocanegra');
+  isUpper = signal(false);
+  heroes = signal(heroes);
 
+  toggleCase() {
+    this.isUpper.set(!this.isUpper());
+  }
 }
